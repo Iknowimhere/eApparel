1e315669d9a53cf2db8b1cb63ba3b8049fd56a08
umashankar <umashankarp33@gmail.com>
2024-01-01 16:21:04 +0530

    add product upate and delete feature

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -36,17 +36,131 @@ export const createProduct = expressAsyncHandler(async (req, res) => {
 // @route  GET /api/v1/products
 // @access Public
 export const getProducts = expressAsyncHandler(async (req, res) => {
-  const { name } = req.query;
   let productQuery = Product.find();
   //search by name
   if (req.query.name) {
-    productQuery = await productQuery.find({
-      name: { $regex: name, $options: "i" },
+    productQuery = productQuery.find({
+      name: { $regex: req.query.name, $options: "i" },
     });
   }
+  //filter by brand
+  if (req.query.brand) {
+    productQuery = productQuery.find({
+      brand: { $regex: req.query.brand, $options: "i" },
+    });
+  }
+  //filter by category
+  if (req.query.category) {
+    productQuery = productQuery.find({
+      category: { $regex: req.query.category, $options: "i" },
+    });
+  }
+  //filter by colors
+  if (req.query.colors) {
+    productQuery = productQuery.find({
+      colors: { $regex: req.query.colors, $options: "i" },
+    });
+  }
+  // //filter by size
+  if (req.query.sizes) {
+    productQuery = productQuery.find({
+      sizes: { $regex: req.query.sizes, $options: "i" },
+    });
+  }
+  //filter by price range
+  if (req.query.price) {
+    let priceRange = req.query.price.split("-");
+    productQuery = productQuery.find({
+      price: { $gte: priceRange[0], $lte: priceRange[1] },
+    });
+  }
+  //pagination
+  //page
+  let page = parseInt(req.query.page) ? parseInt(req.query.page) : 1;
+  //limit
+  let limit = parseInt(req.query.limit) ? parseInt(req.query.limit) : 10;
+  //startIndex
+  let startIdx = (page - 1) * limit;
+  //endIndex
+  let endIdx = page * limit;
+  //total
+  let total = await Product.countDocuments();
+  productQuery = productQuery.skip(startIdx).limit(endIdx);
+  //pagination
+  let pagination = {};
+  if (endIdx < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+  if (startIdx > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+  //await the query
   let products = await productQuery;
   res.json({
     status: "Success",
+    total,
+    results: products.length,
+    pagination,
+    message: "Fetched products successfully",
     products,
+  });
+});
+
+// @desc   Get Single Product
+// @route  GET /api/v1/products/:id
+// @access Public
+
+export const getProduct = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  if (!product) {
+    throw new Error("There's no product");
+  }
+  res.json({
+    status: "Success",
+    message: "Product fetched successfully",
+    product,
+  });
+});
+
+// @desc   Update Product
+// @route  PUT /api/v1/products/:id
+// @access Private/Admin
+export const updateProduct = expressAsyncHandler(async (req, res) => {
+  const { name, description, brand, category, sizes, colors, price, totalQty } =
+    req.body;
+  const { id } = req.params;
+  const updatedProduct = await Product.findByIdAndUpdate(id, {
+    name,
+    description,
+    brand,
+    category,
+    sizes,
+    colors,
+    price,
+    totalQty,
+  },{new:true});
+  res.json({
+    status: "success",
+    message: "Successfully product updated",
+    updatedProduct,
+  });
+});
+
+// @desc   Delete Product
+// @route  DELETE /api/v1/products/:id
+// @access Private/Admin
+export const deleteProduct = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndDelete(id);
+  res.json({
+    status: "success",
+    message: "Successfully product deleted",
   });
 });
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,11 +2,17 @@ import express from "express";
 import { auth } from "../middlewares/authMiddleware.js";
 import {
   createProduct,
+  deleteProduct,
+  getProduct,
   getProducts,
+  updateProduct,
 } from "../controllers/productControllers.js";
 let productRoutes = express.Router();
 
 productRoutes.post("/", auth, createProduct);
 productRoutes.get("/", getProducts);
+productRoutes.get("/:id", getProduct);
+productRoutes.put("/:id", auth, updateProduct);
+productRoutes.delete("/:id", auth, deleteProduct);
 
 export default productRoutes;
